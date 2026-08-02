5ef88468a08e29f1a291415675a344aacc65e4cc
Jeeva k <jeevakrishnan2505@gmail.com>
2026-08-02 23:40:01 +0530

    Initial commit - Module 1 setup

---
+++ src/main/java/com/loanmanagement/application/Main.java
@@ -0,0 +1,28 @@
+package com.loanmanagement.application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/fxml/login.fxml"));
+
+        Scene scene = new Scene(loader.load(), 500, 450);
+
+        stage.setTitle("Loan Management System");
+
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
